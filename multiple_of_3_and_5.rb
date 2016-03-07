326461f684277c2c9794cbeb2df96e3b958cf703
Kalyani Kirad <kiradkalyani@gmail.com>
2016-03-07 19:30:13 +0530

    Refactor code to calculate sum of multiples of 3 & 5

--- multiple_of_3_and_5.rb
+++ multiple_of_3_and_5.rb
@@ -1,10 +1,5 @@
 def divisible_by_3_and_5(numbers)
-  sum = 0
-  numbers.each do |num|
-    if num % 3 == 0 || num % 5 == 0
-      sum = sum + num
-    end
-  end
-  puts "Sum is #{sum}"
+  arr = numbers.select {|num| num % 3 ==0 || num % 5 == 0} 
+  puts " Sum is: #{arr.inject(:+)}"
 end
 divisible_by_3_and_5(1..999)
