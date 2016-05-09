dffb067af0f37fd38c8cf543090fbf3b1c4a9cf7
Kalyani Kirad <kiradkalyani@gmail.com>
2016-05-09 22:45:32 +0530

    Update multiple_of_3_and_5.rb
    Added Problem Statement

--- multiple_of_3_and_5.rb
+++ multiple_of_3_and_5.rb
@@ -1,3 +1,6 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
 def divisible_by_3_and_5(numbers)
   arr = numbers.select {|num| num % 3 ==0 || num % 5 == 0} 
   puts " Sum is: #{arr.inject(:+)}"
